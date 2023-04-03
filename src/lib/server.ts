873d6a5a2238298c257e3d5569d53ccdb32c899e
Ondřej Synáček <ondrej@synacek.org>
2023-04-03 22:12:51 +0200

    set up fastify and node

---
+++ src/lib/server.ts
@@ -0,0 +1,18 @@
+import fastify from 'fastify';
+
+const PORT = Number(process.env.PORT) || 3000;
+
+const server = fastify();
+
+server.get('/ping', async (request, reply) => {
+  return 'pong\n';
+});
+
+server.listen({ port: PORT }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
+
