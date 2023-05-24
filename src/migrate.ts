bf809cce69007647672667c10c58180e17ac1c47
Ondřej Synáček <ondrej@synacek.org>
2023-05-24 21:34:35 +0200

    use sqlite for migrations as well

--- src/migrate.ts
+++ src/migrate.ts
@@ -1,7 +1,9 @@
 import { join } from 'path';
 
 import Postgrator from 'postgrator';
-import { Database, verbose } from 'sqlite3';
+import sqlite3 from 'sqlite3';
+import {  open } from 'sqlite';
+import type { Database } from 'sqlite';
 import yargs from 'yargs';
 
 const cliOptions = yargs(process.argv.slice(2))
@@ -18,38 +20,24 @@ const cliOptions = yargs(process.argv.slice(2))
   .help()
   .parseSync();
 
-const list = (client: Database) => {
-  client.all<{
+const list = async (client: Database<sqlite3.Database, sqlite3.Statement>) => {
+  const rows = await client.all<{
     name: string;
     version: string;
     md5: string;
-  }>('SELECT * FROM migrations', (err, rows) => {
-    if (err) {
-      throw err;
-    }
+  }[]>('SELECT * FROM migrations;');
 
-    for (const row of rows) {
-      console.log(`${row.md5}: ${row.name}`);
-    }
-  });
+  for (const row of rows) {
+    console.log(`${row.md5}: ${row.name}`);
+  }
 };
 
-const migrate = async (client: Database, version?: string) => {
+const migrate = async (client: Database<sqlite3.Database, sqlite3.Statement>, version?: string) => {
   const postgrator = new Postgrator({
     driver: 'sqlite3',
     migrationPattern: join(__dirname, 'lib', '/migrations/*.sql'),
     schemaTable: 'migrations',
-    execQuery: async (query) => {
-      return new Promise((resolve, reject) => {
-        client.all(query, (err, rows) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve({ rows });
-          }
-        });
-      });
-    }
+    execQuery: (query) => client.all(query),
   });
 
   const result = await postgrator.migrate(version);
@@ -67,8 +55,11 @@ const migrate = async (client: Database, version?: string) => {
 };
 
 void (async (options: typeof cliOptions) => {
-  verbose();
-  const client = new Database(join(__dirname, '..', 'sqlite.db'));
+  sqlite3.verbose();
+  const client = await open<sqlite3.Database, sqlite3.Statement>({
+    filename: join(__dirname, '..', 'sqlite.db'),
+    driver: sqlite3.Database,
+  });
 
   try {
     if (options.list) {
