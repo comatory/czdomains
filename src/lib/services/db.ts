5bbbd76c69c7885f116f747cefd58cb8211e4665
Ondřej Synáček <ondrej@synacek.org>
2023-05-28 21:29:23 +0200

    fix path to database file

--- src/lib/services/db.ts
+++ src/lib/services/db.ts
@@ -1,11 +1,12 @@
 import { join } from 'node:path';
+import { cwd } from 'node:process';
 
 import { open } from 'sqlite';
 import sqlite3 from 'sqlite3';
 
 export async function createDatabase() {
   const db = await open({
-    filename: join(__dirname, '..', 'sqlite.db'),
+    filename: join(cwd(), 'sqlite.db'),
     driver: sqlite3.Database,
   });
 
