cc6e5a16fa960aa9ff0f4eba9b58a4dd3069f29f
Ondřej Synáček <ondrej@synacek.org>
2023-06-22 16:17:27 +0200

    translations for home page

--- src/lib/utils/intl.ts
+++ src/lib/utils/intl.ts
@@ -12,6 +12,26 @@ const localisations = {
           'Simple database of Czechia TLD domains which were registered in the past. You can search by its name to get its detail.',
         description:
           "You can retrieve domain's {link} status and WHOIS data but {emStart}beware the capacity is limited{emClose} when doing so. Submissions are welcome if the domain is not already in the database. Each submission is reviewed by the administrator.",
+        faq: {
+          tldHistoryQuestion: 'What is the history of the TLD?',
+          tldHistoryAnswer:
+            'Originally the TLD used was .cs and it was introduced on June 4th 1991. See {source}.',
+          tldCreateQuestion: 'When was the .cz TLD created?',
+          tldCreateAnswer:
+            'The .cz TLD was created on January 13th 1993. See {source}.',
+          firstCzWebsiteQuestion: 'What was the first .cz website?',
+          firstCzWebsiteAnswer:
+            'The first .cz website belonged to czech community of high energy particles physicists. The website launched on June 6th 1993. See the reconstructed version here: {source}.',
+        },
+        attributions: {
+          title: 'Attributions',
+          whois: 'czdomains is using IP2Location.io {source} web service.',
+          icons: 'The icons come from {source}.',
+        },
+        footer: {
+          lastUpdate: 'Latest addition: {date}.',
+          language: 'Language',
+        },
       },
     },
     cz: {
@@ -20,6 +40,26 @@ const localisations = {
           'Jednoduchá databáze domén s českou koncovkou, které byly v minulosti registrovány. Můžete vyhledávat podle názvu, abyste získali podrobnosti.',
         description:
           'Můžete získat archivní informace z {link} a WHOIS data, ale {emStart}tato činnost je kapacitně omezena{emClose}. Pokud chcete do databáze přidat novou doménu, jste vítáni, ale každý příspěvek musí být manuálně schválen.',
+        faq: {
+          tldHistoryQuestion: 'Jaká je historie domény?',
+          tldHistoryAnswer:
+            'Původně se používala TLD .cs a vznikí 4. června 1991. Odkaz: {source}.',
+          tldCreateQuestion: 'Kdy vznikla .cz TLD doména?',
+          tldCreateAnswer:
+            '.cz doména prvního řádu vzniká 13. ledna 1993. Odkaz: {source}.',
+          firstCzWebsiteQuestion: 'Co byla první česká WWW stránka?',
+          firstCzWebsiteAnswer:
+            'První česká WWW stránka patřila české komunitě fyziků vysokých energií. Stránka byla spuštěna 6. června 1993. Rekonstruovanou verzi můžete vidět zde: {source}.',
+        },
+        attributions: {
+          title: 'Zdoje',
+          whois: 'czdomains používají IP2Location.io {source} službu.',
+          icons: 'Ikony pochází z {source}.',
+        },
+        footer: {
+          lastUpdate: 'Poslední příspevěk: {date}.',
+          language: 'Jazyk',
+        },
       },
     },
   },
